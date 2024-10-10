d48494fb24ffefd9db75001fe16e5708207f1bb6
amandaacs <amanda_cs@aol.com>
2024-10-09 23:02:36 -0300

    updated bcrypt install

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,4 +1,4 @@
-const bcrypt = require('bcryptjs/dist/bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
 const createError = require('../utils/appError');
